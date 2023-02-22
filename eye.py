bcb1945fdbb9270273ee8c48ec7a4022b3afc63b
Abhishek S R <abhishekreju007@gmail.com>
2023-02-22 20:51:56 +0530

    Reading live feed from webcam using opencv

---
+++ eye.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+cap = cv2.VideoCapture(0)
+ret, img = cap.read()
+thresh = img.copy()
+cv2.namedWindow('Live')
+kernel = np.ones((9, 9), np.uint8)
+
+
+def nothing(x):
+    pass
+
+
+while (True):
+    ret, img = cap.read()
+    cv2.imshow('Live', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
