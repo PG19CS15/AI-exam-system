b38923dcdfa555dc9fd90e3517d7ae7abf74156a
Amaldev538 <amaldev538@gmail.com>
2023-02-23 22:13:12 +0530

    Image converted to grayscale for thresholding

--- eye.py
+++ eye.py
@@ -38,16 +38,22 @@ mask = eye_on_mask(mask, left)
 mask = eye_on_mask(mask, right)
 
 while (True):
-    img = cv2.imread('image.png')
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert to grayscale
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     for (i, rect) in enumerate(rects):
         shape = predictor(gray, rect)
         shape = shape_to_np(shape)
         for (x, y) in shape:
             cv2.circle(img, (x, y), 2, (0, 0, 255), -1)
 
-    rects = detector(gray, 1)  # rects contains all the faces detected
-    ret, img = cap.read()
+    rects = detector(gray, 1)
+    for rect in rects:
+        kernel = np.ones((9, 9), np.uint8)
+        mask = cv2.dilate(mask, kernel, 5)
+        eyes = cv2.bitwise_and(img, img, mask=mask)
+        mask = (eyes == [0, 0, 0]).all(axis=2)
+        eyes[mask] = [255, 255, 255]
+        eyes_gray = cv2.cvtColor(eyes, cv2.COLOR_BGR2GRAY)
     cv2.imshow('Live', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
