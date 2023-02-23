ddce60f1fa2f2e92374817c6db0b1c3c459dd0d8
Iwin Francis <iwintomfrancis@gmail.com>
2023-02-23 20:45:56 +0530

    detecting frontal face

--- eye.py
+++ eye.py
@@ -1,5 +1,7 @@
-import cv2
 import numpy as np
+import cv2
+import dlib
+
 cap = cv2.VideoCapture(0)
 ret, img = cap.read()
 thresh = img.copy()
@@ -11,7 +13,13 @@ def nothing(x):
     pass
 
 
+detector = dlib.get_frontal_face_detector()
+
 while (True):
+    img = cv2.imread('image.png')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert to grayscale
+
+    rects = detector(gray, 1)  # rects contains all the faces detected
     ret, img = cap.read()
     cv2.imshow('Live', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
