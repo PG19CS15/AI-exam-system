d03f9918d763348d2108d7f2c51c32e3f3981cae
Amaldev538 <amaldev538@gmail.com>
2023-02-23 23:05:00 +0530

    A trackbar is added to adjust threshold value

--- eye.py
+++ eye.py
@@ -1,59 +1,51 @@
+import cv2
 import numpy as np
 import cv2
 import dlib
-
-
 def shape_to_np(shape, dtype="int"):
     coords = np.zeros((68, 2), dtype=dtype)
     for i in range(0, 68):
         coords[i] = (shape.part(i).x, shape.part(i).y)
     return coords
-
-
 def eye_on_mask(mask, side):
     points = [shape[i] for i in side]
     points = np.array(points, dtype=np.int32)
     mask = cv2.fillConvexPoly(mask, points, 255)
     return mask
-
-
-
 cap = cv2.VideoCapture(0)
 ret, img = cap.read()
 thresh = img.copy()
 cv2.namedWindow('Live')
 kernel = np.ones((9, 9), np.uint8)
-
-
 def nothing(x):
     pass
-
-
+cv2.namedWindow('image')
+cv2.createTrackbar('threshold', 'image', 0, 255, nothing)
 predictor = dlib.shape_predictor('shape_68.dat')
 detector = dlib.get_frontal_face_detector()
 left = [36, 37, 38, 39, 40, 41]  # keypoint indices for left eye
 right = [42, 43, 44, 45, 46, 47]  # keypoint indices for right eye
-mask = np.zeros(img.shape[:2], dtype=np.uint8)
-mask = eye_on_mask(mask, left)
-mask = eye_on_mask(mask, right)
-
 while (True):
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    for (i, rect) in enumerate(rects):
-        shape = predictor(gray, rect)
-        shape = shape_to_np(shape)
-        for (x, y) in shape:
-            cv2.circle(img, (x, y), 2, (0, 0, 255), -1)
-
     rects = detector(gray, 1)
     for rect in rects:
-        kernel = np.ones((9, 9), np.uint8)
+        shape = predictor(gray, rect)
+        shape = shape_to_np(shape)
+        mask = np.zeros(img.shape[:2], dtype=np.uint8)
+        mask = eye_on_mask(mask, left)
+        mask = eye_on_mask(mask, right)
         mask = cv2.dilate(mask, kernel, 5)
         eyes = cv2.bitwise_and(img, img, mask=mask)
         mask = (eyes == [0, 0, 0]).all(axis=2)
         eyes[mask] = [255, 255, 255]
+        mid = (shape[42][0] + shape[39][0]) // 2
         eyes_gray = cv2.cvtColor(eyes, cv2.COLOR_BGR2GRAY)
+        threshold = cv2.getTrackbarPos('threshold', 'image')
+        _, thresh = cv2.threshold(eyes_gray, threshold, 255, cv2.THRESH_BINARY)
+        thresh = cv2.erode(thresh, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=4)
+        thresh = cv2.medianBlur(thresh, 3)
     cv2.imshow('Live', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
