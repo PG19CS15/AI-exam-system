1cb8fe0a4a5632cb573169dc04c25476eac45927
amaldev538 <amaldev538@gmail.com>
2023-03-01 22:56:28 +0530

    removed unused codes

--- face_realtime.py
+++ face_realtime.py
@@ -1,14 +1,10 @@
 import cv2
 import numpy as np
 from deepface import DeepFace
-cap = cv2.VideoCapture(0)
-ret, img = cap.read()
-thresh = img.copy()
-cv2.namedWindow('Live')
-kernel = np.ones((9, 9), np.uint8)
 
-def nothing(x):
-    pass
+cap = cv2.VideoCapture(0)
+
+cv2.namedWindow('Live')
 
 while (True):
     ret, img = cap.read()
@@ -18,6 +14,6 @@ while (True):
     print("Face verification : ", obj['verified'])
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 cap.release()
 cv2.destroyAllWindows()
-
