8ff539318adc060b5dd3cdb4eb8f6bdfa027af1d
amaldev538 <amaldev538@gmail.com>
2023-03-28 01:25:09 +0530

    Descriptive Answer Evaluation

---
+++ descranseval.py
@@ -0,0 +1,4 @@
+import nltk
+sent = "There once lived an old man and an old woman who were peasants and had to work hard to earn their daily bread. The old man used to go to fix fences and do other odd jobs for the farmers around, and while he was gone the old woman, his wife, did the work of the house and worked in their own little plot of land."
+token=nltk.wordpunct_tokenize(sent)
+print(token)
