d12f82bee30dff058a376cac2c1c11747e9a8a5d
Abhishek S R <abhishekreju007@gmail.com>
2023-03-28 13:15:27 +0530

    Live feed streaminig on web app

--- AI_exam_system/examApp/views.py
+++ AI_exam_system/examApp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import cv2
+from django.http import StreamingHttpResponse
+from django.views.decorators import gzip
 
-# Create your views here.
+
+@gzip.gzip_page
+def live_feed(request):
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        _, jpeg = cv2.imencode('.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+    cap.release()
+
+
+def index(request):
+    return StreamingHttpResponse(live_feed(request),
+                                 content_type='multipart/x-mixed-replace; boundary=frame')
